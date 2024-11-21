12e7aed528758f54e1f51df66b562fbf7d4af4f3
juneyseoky <juneseokoh@gmail.com>
2024-11-21 09:48:12 +0900

    input blur empty값 처리

--- P01_QD_Join/src/main/resources/static/script/script.js
+++ P01_QD_Join/src/main/resources/static/script/script.js
@@ -1,9 +1,21 @@
 $(function () {
-	$("input").blur(function(){
-		let val = $(this).val().trim();
-		if(val == ""){
+	$("input").on("focusin",function(){
+		$(this).parent().css("border-color", "#2DB400");
+	})
+	$("input").on("focusout",function(){
+		if($(this).val().trim() == ""){
 			$(this).attr("placeholder","비어있습니다.");
-			$(this).focus();
+			$(this).parent().css("border-color", "red");
+		}else{		
+			$(this).parent().css("border-color", "#aaa");
 		}
 	})
+
+//	$("input").blur(function(){
+//		let val = $(this).val().trim();
+//		if(val == ""){
+//			$(this).attr("placeholder","비어있습니다.");
+//			$(this).focus();
+//		}
+//	})
 });
